270e5ba5f4586b9cdcfc9b86d7dff2f8fc34a6d6
yuhyeahyah <124215060+yuhyeahyah@users.noreply.github.com>
2025-01-06 20:00:55 -0300

    Update and rename config.py to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "7343343637:AAGwysmJmkLjiOotMrgkHphtxeRCZs12xOw", // Substitua com o token real do seu bot
+  WEBHOOK_URL: "SUA_URL_WEBHOOK", // Substitua com a sua URL de webhook
+  PORT: 8080, // Porta do seu webhook
+  DATABASE_FILE: "db.sqlite",
+};
