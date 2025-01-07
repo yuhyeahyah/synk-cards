5baad43859676a39ba196e1de84ac3ec179c5ee7
yuhyeahyah <124215060+yuhyeahyah@users.noreply.github.com>
2025-01-07 02:02:39 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   TOKEN: "7343343637:AAGwysmJmkLjiOotMrgkHphtxeRCZs12xOw", // Substitua com o token real do seu bot
   WEBHOOK_URL: "SUA_URL_WEBHOOK", // Substitua com a sua URL de webhook
-  PORT: 8080, // Porta do seu webhook
+  PORT: 3000, // Porta do seu webhook
   DATABASE_FILE: "db.sqlite",
   ADMIN_ID: 1889562226, // Substitua com o id do seu usuário do telegram
 };
